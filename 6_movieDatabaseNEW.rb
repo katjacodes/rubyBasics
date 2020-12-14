a8af22a7ef80dd445411eb6aeb42b5d1a867ca9d
katjacodes <katjacodes@gmail.com>
2020-12-14 16:45:06 +0100

    ...with a little help from my friends... found a way to store the movie titles in symbols & still let users access them by enteriing strings

---
+++ 6_movieDatabaseNEW.rb
@@ -0,0 +1,58 @@
+=begin
+Some code to help you store your movie ratings. 
+Would've loved to get it to work with symbols
+instead of hashrockets, but .to_sym is not my 
+friend right now. More later.
+=end
+
+movies = {
+    Great_Expectations: 5, 
+    }
+  
+  puts "What would you like to do?"
+  puts "'add' to add a movie" 
+  puts "'update' to update an existing movie rating" 
+  puts "'delete' to delete a movie" 
+  puts "'display' to display your entire list of movies"
+  choice = gets.chomp
+  
+  case choice
+  when "add"
+    puts "What movie would you like to add? "
+    title = gets.split(' ').join('_').to_sym
+    if movies[title].nil?
+      puts "What is your rating? (Type a number 0 to 5.) "
+      rating = gets.chomp
+      movies[title] = rating.to_i
+      puts "#{title} has been added with a rating of #{rating}."
+   else
+    puts "That movie already exists in your database! Its rating is #{movies[title]}."
+   end
+
+  when "update"
+    puts "What movie rating would you like to update? "
+    title = gets.chomp.split(' ').join('_').to_sym
+    if movies[title].nil?
+      puts "Error! That movie does not exist in your database."
+    else
+      puts "Update your rating! Type a number 0 to 5.) "
+      rating = gets.chomp
+      movies[title] = rating.to_i
+      puts "The rating of #{title} has been updated. It is now #{rating}."
+    end
+    
+  when "display"
+    movies.each { |movie, rating| puts "#{movie}: #{rating}" }
+  
+  when "delete"
+    puts "Which movie would you like to delete? "
+    title = gets.split(' ').join('_').to_sym
+      if movies[title].nil?
+        puts "Error! That movie does not exist in your database."
+      else
+        movies.delete(title)
+      end
+
+  else
+    puts "Error! This is not a valide command."
+  end
